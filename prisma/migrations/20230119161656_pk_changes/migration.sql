bae4cdf000e61505e509bacde8973d48afa0c29d
Paulo Serrano <pvsserrano@gmail.com>
2023-01-19 13:17:35 -0300

    prisma updated

---
+++ prisma/migrations/20230119161656_pk_changes/migration.sql
@@ -0,0 +1,52 @@
+/*
+  Warnings:
+
+  - The primary key for the `Session` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Session` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `Todo` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Todo` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - You are about to alter the column `email` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `password` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - Changed the type of `userId` on the `Session` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `userId` on the `Todo` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Todo" DROP CONSTRAINT "Todo_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Session" DROP CONSTRAINT "Session_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+DROP COLUMN "userId",
+ADD COLUMN     "userId" INTEGER NOT NULL,
+ADD CONSTRAINT "Session_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Todo" DROP CONSTRAINT "Todo_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+DROP COLUMN "userId",
+ADD COLUMN     "userId" INTEGER NOT NULL,
+ADD CONSTRAINT "Todo_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ALTER COLUMN "email" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "password" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "name" SET DATA TYPE VARCHAR(255),
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
